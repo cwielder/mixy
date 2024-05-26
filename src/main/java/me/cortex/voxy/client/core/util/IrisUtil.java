861b3ebb5396eb6e1cbec0e6f0b36bbac1397737
ferriarnus <61201275+ferriarnus@users.noreply.github.com>
2024-05-26 14:52:51 +0200

    update iris

--- src/main/java/me/cortex/voxy/client/core/util/IrisUtil.java
+++ src/main/java/me/cortex/voxy/client/core/util/IrisUtil.java
@@ -1,6 +1,6 @@
 package me.cortex.voxy.client.core.util;
 
-import net.coderbot.iris.pipeline.ShadowRenderer;
+import net.irisshaders.iris.shadows.ShadowRenderer;
 import net.neoforged.fml.ModList;
 import net.neoforged.fml.loading.LoadingModList;
 
